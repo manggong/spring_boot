d9b9aa8af2b666e952e382b795d332d2ab09d17f
manggong <jihwank1995@gmail.com>
2021-12-09 18:19:42 +0900

    feat: Add Entity, testCode, libraries

---
+++ shop/src/main/java/com/example/shop/repository/ItemRepository.java
@@ -0,0 +1,19 @@
+package com.example.shop.repository;
+
+import com.example.shop.entity.Item;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+
+public interface ItemRepository extends JpaRepository<Item, Long> {
+
+    List<Item> findByItemNm(String itemNm);
+    List<Item> findByItemNmOrItemDetail(String itemNm, String itemDetail);
+    List<Item> findByPriceLessThan(Integer price);
+    List<Item> findByLessThanOrderByPriceDesc(Integer price);
+
+    @Query("select i from Item i where i.itemDetail like %:itemDetail% order by i.price desc")
+    List<Item> findByItemDetail(@Param("itemDetail") String itemDetail);
+}
