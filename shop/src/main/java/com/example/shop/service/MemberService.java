1b045f19aa2f36f6b31cf2f81be2caf87067d1f6
manggong <jihwank1995@gmail.com>
2021-12-10 20:45:27 +0900

    feat: Add DTO, Controllers, MemberService

---
+++ shop/src/main/java/com/example/shop/service/MemberService.java
@@ -0,0 +1,27 @@
+package com.example.shop.service;
+
+import com.example.shop.entity.Member;
+import com.example.shop.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class MemberService {
+
+    private final MemberRepository memberRepository;
+
+    public Member saveMember(Member member) {
+        validateDuplicateMember(member);
+        return memberRepository.save(member);
+    }
+
+    public void validateDuplicateMember(Member member) {
+        Member findMember = memberRepository.findByEmail(member.getEmail());
+        if(findMember != null) {
+            throw new IllegalStateException("이미 가입된 회원입니다.");
+        }
+    }
+}
