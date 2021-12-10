5e8e120b9a118e1e366c2bd94a31c85ac361fad9
manggong <jihwank1995@gmail.com>
2021-12-10 16:16:08 +0900

    feat: Apply thymeleaf

--- shop/src/main/java/com/example/shop/repository/ItemRepository.java
+++ shop/src/main/java/com/example/shop/repository/ItemRepository.java
@@ -10,10 +10,10 @@ import java.util.List;
 public interface ItemRepository extends JpaRepository<Item, Long> {
 
     List<Item> findByItemNm(String itemNm);
-    List<Item> findByItemNmOrItemDetail(String itemNm, String itemDetail);
-    List<Item> findByPriceLessThan(Integer price);
-    List<Item> findByLessThanOrderByPriceDesc(Integer price);
-
-    @Query("select i from Item i where i.itemDetail like %:itemDetail% order by i.price desc")
-    List<Item> findByItemDetail(@Param("itemDetail") String itemDetail);
+//    List<Item> findByItemNmOrItemDetail(String itemNm, String itemDetail);
+//    List<Item> findByPriceLessThan(Integer price);
+//    List<Item> findByLessThanOrderByPriceDesc(Integer price);
+//
+//    @Query("select i from Item i where i.itemDetail like %:itemDetail% order by i.price desc")
+//    List<Item> findByItemDetail(@Param("itemDetail") String itemDetail);
 }
