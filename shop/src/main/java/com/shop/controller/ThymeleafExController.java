9287ca0482fc9e22e8ae069ab27603d9ae7d46d4
manggong <jihwank1995@gmail.com>
2022-02-13 14:52:17 +0900

    feat: Add layout

--- shop/src/main/java/com/shop/controller/ThymeleafExController.java
+++ shop/src/main/java/com/shop/controller/ThymeleafExController.java
@@ -1,9 +1,12 @@
 package com.shop.controller;
 
+import com.shop.dto.ItemDto;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.time.LocalDateTime;
 
 @Controller
 @RequestMapping(value = "/thymeleaf")
@@ -12,6 +15,23 @@ public class ThymeleafExController {
     public String thymeleafExample01(Model model) {
         model.addAttribute("data", "타임리프 예제 입니다.");
         return "thymeleafEx/thymeleafEx01";
+    }
+
+    @GetMapping(value = "/ex02")
+    public String thymeleafExample02(Model model) {
+        ItemDto itemDto = new ItemDto();
+        itemDto.setItemDetail("test stuff desc");
+        itemDto.setItemNm("test stuff1");
+        itemDto.setPrice(10000);
+        itemDto.setRegTime(LocalDateTime.now());
+
+        model.addAttribute("itemDto", itemDto);
+        return "thymeleafEx/thymeleafEx02";
+    }
+
+    @GetMapping(value = "/ex07")
+    public String thymeleafExample07(Model model) {
+        return "thymeleafEx/thymeleafEx07";
     }
 
 }
